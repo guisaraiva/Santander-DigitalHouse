13056d344af3858ea9b1e8a5789fe48d14aabe5b
Guilherme Saraiva <guilhermesilva93@hotmail.com>
2020-08-03 22:58:56 -0300

    atualização de atividades feitas na aula.

--- Aulas/03082020/ajax/main.js
+++ Aulas/03082020/ajax/main.js
@@ -1,7 +1,31 @@
+const config = {
+    method: "GET",
+    url: "cidades.json",
+    sucess: function(response){
+        const cidades = JSON.parse(response);
+        console.log(cidades);
+    },
+    error: function(err){
+        console.log(...err);
+    }
+};
+
 function ajax (config) {
     const xhr = new XMLHttpRequest();
-    xhr.open(config.method, config.url, true)
+    xhr.open(config.method, config.url, true);
 
-    shr
-}
+    xhr.onload = e => {
+        if (xhr.status >= 200 &&  xhr.status < 300){
+            config.sucess(xhr.response)
+        }
+        else if (xhr.status >= 400){
+            config.error({
+                code: xhr.status,
+                text: xhr.statusText
+            });
+        }
+    }
+    xhr.send();
+};
 
+ajax(config)
