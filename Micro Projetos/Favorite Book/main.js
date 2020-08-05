eb9fb56630cdfe757761376b5b56434c4803f79c
Guilherme Saraiva <guilhermesilva93@hotmail.com>
2020-08-04 22:33:21 -0300

    Atualização do miniprojeto favorite books

---
+++ Micro Projetos/Favorite Book/main.js
@@ -0,0 +1,35 @@
+window.addEventListener('load', start);
+
+function start(){
+    console.log('Página totalmente carregada...');
+
+    showSlides();
+    
+    
+}
+
+function exibeCaractere(){
+    let bookInput = document.getElementById('search_bar').value;
+    console.log(bookInput);
+}
+
+/*Function carrossel*/
+let index = 0;
+;
+function showSlides() {
+    let count;
+    let slides = document.getElementsByClassName("mySlides");
+    for (count = 0; count < slides.length; count++) {
+        slides[count].style.display = "none";
+    }
+    index++;
+    if (index > slides.length) { 
+        index = 1 
+    }
+    slides[index - 1].style.display = "block";
+    setTimeout(showSlides, 3000);
+}
+
+let getBooks = document.getElementById("search_bar")
+console.log(getBooks.value)
+
