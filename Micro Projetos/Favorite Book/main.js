0902e3edbc84905b52bfaf2c14efefca9a19ec34
Guilherme Saraiva <guilhermesilva93@hotmail.com>
2020-08-07 00:13:34 -0300

    Atualização do mini projeto de lista de livros

--- Micro Projetos/Favorite Book/main.js
+++ Micro Projetos/Favorite Book/main.js
@@ -1,17 +1,50 @@
-window.addEventListener('load', start);
+let search_bar = null;
+let search_button = null;
+let conteudo = '';
+let listBooks ;
 
-function start(){
-    console.log('Página totalmente carregada...');
+let acervo = document.querySelector('.showcase');
+
+window.addEventListener('load', ()=>{
     showSlides();
-    
+    //renderAllBooks();
+    mapElements();
+    addEvents();
+})
+
+listBooks = books.map(({titulo, sinopse, autor}) =>{
+    return {
+        titulo: titulo,
+        tituloLowerCase: titulo.toLowerCase(),
+        sinopse,
+        autor,
+    }
+}).sort((a,b)=> {
+    return a.titulo.localeCompare(b.titulo);
+});
+
+
+function renderAllBooks () {
+
+    listBooks.forEach(book => {
+        conteudo += 
+        `<div class="card">
+            <img class = "formated_img" 
+            src="https://cdn.pixabay.com/photo/2017/10/29/11/33/fantasy-2899517_960_720.jpg">
+            <div class="overlay">
+            <h2>${book.titulo}</h2>
+            <p>${book.sinopse}</p>
+            </div>
+        </div>`
+});
+    acervo.innerHTML = conteudo;
+
 }
 
-function exibeCaractere(){
-    let bookInput = document.getElementById('search_bar').value;
-    console.log(bookInput);
-}
 
-/*Function carrossel*/
+
+
+/* --------------------Function carrossel ----------------------*/
 let index = 0;
 ;
 function showSlides() {
@@ -21,13 +54,69 @@ function showSlides() {
         slides[count].style.display = "none";
     }
     index++;
-    if (index > slides.length) { 
+    if (index > slides.length) {     
         index = 1 
     }
     slides[index - 1].style.display = "block";
     setTimeout(showSlides, 3000);
 }
 
-let getBooks = document.getElementById("search_bar")
-console.log(getBooks.value)
+/** ----------------------------------------------------------- */
 
+function mapElements(){
+    search_bar = document.querySelector('#search_bar');
+    search_button = document.querySelector('#search_button');
+
+};
+
+
+function addEvents(){
+    search_bar.addEventListener('keyup', handleKeyup);
+}
+
+function handleKeyup (event) {
+    const currentKey = event.key;
+
+    if ( currentKey !== 'Enter'){
+        acervo.innerHTML='';
+        return;
+    }
+
+    const filterText = event.target.value;
+   
+    if( filterText.trim() !== ''){
+        filterBooks(filterText);
+    }
+
+}
+
+function filterBooks(filterText) {
+
+    const filteredLowerCase = filterText.toLowerCase();
+
+    const filteredBooks = listBooks.filter((book) =>{
+        return book.tituloLowerCase.includes(filteredLowerCase);
+    });
+
+    //console.log(filteredBooks)
+    renderBooks(filteredBooks);
+}
+
+function renderBooks(result){
+    let x ;
+    
+    for(x = 0; x < result.length; x++){
+        conteudo += 
+        `<div class="card">
+            <img class = "formated_img" 
+            src="https://cdn.pixabay.com/photo/2017/10/29/11/33/fantasy-2899517_960_720.jpg">
+            <div class="overlay">
+            <h2>${result[x].titulo}</h2>
+            <p>${result[x].sinopse}</p>
+            </div>
+        </div>`
+    }
+    acervo.innerHTML = conteudo;
+
+
+}
