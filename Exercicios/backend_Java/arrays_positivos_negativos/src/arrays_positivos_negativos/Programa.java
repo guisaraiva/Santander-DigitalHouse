c3d2e3d5cb05d73332e85e25344bbd9ed385c7db
Guilherme Saraiva <guilhermesilva93@hotmail.com>
2020-08-11 23:45:35 -0300

    Execução de alguns exercícios sobre array em Java

---
+++ Exercicios/backend_Java/arrays_positivos_negativos/src/arrays_positivos_negativos/Programa.java
@@ -0,0 +1,22 @@
+package arrays_positivos_negativos;
+
+class Programa {
+
+	public static void main(String[] args) {
+
+		int[] numbers = { 3, -15, 35, -11, 12, 35, 7, -85 };
+		int[] numbersPositives, numberNegatives;
+		numbersPositives = new int[8];
+		numberNegatives = new int[8];
+
+		for (int count = 0; count < numbers.length; count++) {
+			if (numbers[count] > 0) {
+				numbersPositives[count] = numbers[count];
+				System.out.println("Positivo: indice-> " + count + " Nº: " + numbersPositives[count]);
+			} else {
+				numberNegatives[count] = numbers[count];
+				System.out.println("Negativo: indice-> " + count + " Nº: " + numberNegatives[count]);
+			}
+		}
+	}
+}
